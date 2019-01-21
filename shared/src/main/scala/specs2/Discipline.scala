0164faf3bacaf5772d706b0f9e276f8ce9d5b243
Denis Buzdalov <buzdalov@ispras.ru>
2019-01-21 19:13:28 +0300

    Ruleset checks were made to be tabbed according to specs2 custom.

--- shared/src/main/scala/specs2/Discipline.scala
+++ shared/src/main/scala/specs2/Discipline.scala
@@ -11,10 +11,10 @@ import org.specs2.scalacheck.Parameters
 trait Discipline extends ScalaCheck { self: SpecificationLike =>
 
   def checkAll(name: String, ruleSet: Laws#RuleSet)(implicit p: Parameters) = {
-    s"""${ruleSet.name} laws must hold for ${name}""" ^ br ^
+    s"""${ruleSet.name} laws must hold for ${name}""" ^ br ^ t ^
     Fragments.foreach(ruleSet.all.properties.toList) { case (id, prop) =>
        id ! check(prop, p, defaultFreqMapPretty) ^ br
-    }
+    } ^ br ^ bt
   }
 
 }
