bae92fc28d42f78bd6346741eda25f232d866760
Vasil Vasilev <vasil@vasilev.io>
2021-10-19 21:48:57 +0200

    Move the build over to `sbt-spiewak`

--- core/src/test/scala/org/typelevel/discipline/laws.scala
+++ core/src/test/scala/org/typelevel/discipline/laws.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2013-2021 Typelevel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package org.typelevel.discipline
 
 import org.scalacheck.Prop
@@ -50,15 +71,15 @@ trait GroupLaws extends Laws {
     )
 
   class GroupProperties(
-    name: String,
-    parent: Option[RuleSet],
-    props: (String, Prop)*
+      name: String,
+      parent: Option[RuleSet],
+      props: (String, Prop)*
   ) extends DefaultRuleSet(name, parent, props: _*)
 
   class AdditiveProperties(
-    val base: GroupProperties,
-    val parent: Option[RuleSet],
-    val props: (String, Prop)*
+      val base: GroupProperties,
+      val parent: Option[RuleSet],
+      val props: (String, Prop)*
   ) extends RuleSet
       with HasOneParent {
     val name = base.name
@@ -122,9 +143,9 @@ object RingLaws extends GroupLaws {
     )
 
   class MultiplicativeProperties(
-    val base: GroupLaws => GroupLaws#GroupProperties,
-    val parent: Option[MultiplicativeProperties],
-    val props: (String, Prop)*
+      val base: GroupLaws => GroupLaws#GroupProperties,
+      val parent: Option[MultiplicativeProperties],
+      val props: (String, Prop)*
   ) extends RuleSet
       with HasOneParent {
     private val _base = base(RingLaws.this)
@@ -134,11 +155,11 @@ object RingLaws extends GroupLaws {
   }
 
   class RingProperties(
-    val name: String,
-    val al: AdditiveProperties,
-    val ml: MultiplicativeProperties,
-    val parents: Seq[RingProperties],
-    val props: (String, Prop)*
+      val name: String,
+      val al: AdditiveProperties,
+      val ml: MultiplicativeProperties,
+      val parents: Seq[RingProperties],
+      val props: (String, Prop)*
   ) extends RuleSet {
     def bases = Seq("additive" -> al, "multiplicative" -> ml)
   }
